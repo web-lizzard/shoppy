d883a39127cc5fd778750ec8af4008d59dcf2e91
web-lizzard <czarnous.adrian@gmail.com>
2024-02-19 18:59:19 +0100

    feat: add Money value_object

--- app/domain/value_object.py
+++ app/domain/value_object.py
@@ -1,8 +1,36 @@
 from dataclasses import dataclass
+from decimal import Decimal
+from enum import StrEnum
+
+class Currency(StrEnum):
+    PLN = 'PLN'
+    USD = 'USD'
 
 @dataclass(frozen=True)
 class Money:
     amount: int 
-    currency: str = 'PLN'
+    currency: Currency = Currency.PLN
 
+    @classmethod
+    def mint(cls, amount: float | Decimal, currency: Currency = Currency.PLN) -> 'Money':
+        return Money(amount=int(amount * 100), currency=currency)
+
+    def __str__(self) -> str:
+        amount = self.amount / 100
+        return f'{amount:.2f} {self.currency}'
+
+    def __add__(self, other: 'Money') -> 'Money':
+        if self.currency != other.currency:
+            raise ValueError("Adding two money representation with different currency is not allowed!")
+        return self.__class__(amount=self.amount + other.amount, currency=self.currency)
     
+    def __sub__(self, other: 'Money') -> 'Money':
+        if self.currency != other.currency:
+            raise ValueError("Subtracting two money representation with different currency is not allowed!")
+        return self.__class__(amount=self.amount - other.amount, currency=self.currency)
+
+    def __mul__(self, other: int) -> 'Money':
+        return self.__class__(amount=self.amount * other)
+    
+    def __truediv__(self, other: int) -> 'Money':
+        return self.__class__(amount=int(self.amount / other))
---
+++ app/test/unit/test_money.py
@@ -0,0 +1,66 @@
+import pytest
+
+from domain.value_object import Money, Currency
+
+
+def test_money_to_string():
+    m = Money(amount=1050)
+
+    assert str(m) == '10.50 PLN'
+
+def test_money_adding():
+    m1 = Money(amount=100)
+    m2 = Money(amount=100)
+
+    m3 = m1 + m2
+
+    assert m3.amount == 200
+    
+     
+def test_money_subtracting():
+    m1 = Money(amount=100)
+    m2 = Money(amount=50)
+
+    m3 = m1 - m2
+
+    assert m3.amount == 50
+
+def test_money_multiplying_by_int():
+    m1 = Money(amount=100)
+
+    m2 = m1 * 2
+
+    assert m2.amount == 200
+
+
+def test_money_dividing_by_int():
+    m1 = Money(amount=200)
+
+    m2 = m1 / 2 
+
+    assert m2.amount == 100
+
+def test_adding_with_different_currency_raise_exception():
+    m1 = Money(amount=2000, currency=Currency.PLN)
+    m2 = Money(amount=200, currency=Currency.USD)
+
+    with pytest.raises(ValueError) as e:
+        m3 = m1 + m2
+
+        assert str(e) == 'Adding two money representation with different currency is not allowed!'
+
+
+def test_subtracting_with_different_currency_raise_exception():
+    m1 = Money(amount=2000, currency=Currency.PLN)
+    m2 = Money(amount=200, currency=Currency.USD)
+
+    with pytest.raises(ValueError) as e:
+        m3 = m1 - m2
+
+        assert str("Subtracting two money representation with different currency is not allowed!")
+
+
+def test_mint_converting_money_from_float_correctly():
+    m = Money.mint(amount=200)
+
+    assert m.amount == 20000
