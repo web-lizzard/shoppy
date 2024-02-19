74ea4fbf6c75ec2067727856439268f906343f1c
web-lizzard <czarnous.adrian@gmail.com>
2024-02-20 00:42:31 +0100

    feat: add order and order value objects

--- app/domain/value_object.py
+++ app/domain/value_object.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 from decimal import Decimal
 from enum import StrEnum
-from typing import Union
 
 
 class Currency(StrEnum):
---
+++ app/test/unit/test_order.py
@@ -0,0 +1,46 @@
+from domain.entities import order as order_domain
+from domain.entities import product
+from domain.value_object import Money, Quantity
+
+
+def get_order() -> order_domain.Order:
+    return order_domain.Order()
+
+
+def get_product() -> product.Product:
+    return product.Product(
+        name="test-name", quantity=Quantity(3), price=Money.mint(1000)
+    )
+
+
+def test_order_adding_order_info():
+    order = get_order()
+    order_info = order_domain.OrderInfo(address="example", email="test@email.com")
+    order.set_order_info(order_info=order_info)
+
+    assert order_info is order.order_info
+
+
+def test_order_adding_ordered_product():
+    product = get_product()
+    product_in_cart = order_domain.OrderedProduct(quantity=Quantity(1), product=product)
+    order = get_order()
+
+    order.add_product_to_order(product_to_add=product_in_cart)
+
+    assert product_in_cart in order.ordered_products
+
+
+def test_adding_same_product_update_quantity():
+    product = get_product()
+    order = get_order()
+    product_in_cart = order_domain.OrderedProduct(quantity=Quantity(1), product=product)
+    product_in_cart_2 = order_domain.OrderedProduct(
+        quantity=Quantity(1), product=product
+    )
+
+    order.add_product_to_order(product_in_cart)
+    order.add_product_to_order(product_in_cart_2)
+
+    assert len(order.ordered_products) == 1
+    assert order.ordered_products[0].quantity == 2
